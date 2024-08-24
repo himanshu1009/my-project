e8f4b184988191c522caa08114a7d539f35112fe
himanshu1009 <himanshu10092004@gmail.com>
2024-08-24 16:46:07 +0530

    jk

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,6 +4,7 @@ return (
         <div className="flex justify-between  px-10 py-1">
             <img className="w-[8vw]" src="logosns.png"></img>
             <div className="flex items-center">
+                <a href="#" className=" content-center font-medium cursor-pointer mx-6 text-white hover:bg-white hover:rounded-full hover:text-black px-3 h-fit z-10">Members</a>
                 <a href="#" className=" content-center font-medium cursor-pointer mx-6 text-white hover:bg-white hover:rounded-full hover:text-black px-3 h-fit z-10">Life At NITC</a>
                 <a href="#" className=" content-center font-medium cursor-pointer mx-6 text-white hover:bg-white hover:rounded-full hover:text-black px-3 h-fit z-10">Activites</a>
                 <a href="#" className=" content-center font-medium cursor-pointer mx-6 text-white hover:bg-white hover:rounded-full hover:text-black px-3 h-fit z-10">About US</a>
