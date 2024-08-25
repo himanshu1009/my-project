d7c6af253a841fa0cf60c0d9ae1feaf15b193332
himanshu1009 <himanshu10092004@gmail.com>
2024-08-25 13:50:55 +0530

    fd

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,8 @@
 export default function Hero() {
 return (
         <div>
-            <p className="newamst text-6xl text-center font-extralight ">Science And Spirituality Club
-            NIT Calicut</p>
+            <p className="newamst text-7xl text-center font-extralight ">Science And Spirituality Club</p>
+            <p className="newamst text-5xl text-center font-extralight ">NIT Calicut</p>
         </div>
     )
 }
