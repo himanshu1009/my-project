2c8e84f485a926f55abfa2664fb97f918c04508d
himanshu1009 <himanshu10092004@gmail.com>
2024-08-25 13:59:00 +0530

    g

--- src/Components/discription.jsx
+++ src/Components/discription.jsx
@@ -1,8 +1,8 @@
 export default function Descrip() {
 return (
         <div className=" desbgc border-2 bg-gradient-to-br  from-black to-[#ffffff71] mt-16 bg-fixed rounded-2xl w-fit m-auto">
-            <p className="desbg z-10 px-5 rounded-2xl text-[#dfb722] py-10 max-w-[40vw] newamst text-4xl text-center">What is spirituality ?</p>
-            <p className="desbg z-10 px-5 rounded-2xl py-4 max-w-[40vw] newamst text-2xl text-center"> and what is science? We at the Science and Spirituality Club are investigating the intersection of knowledge and awareness in an effort to hunt  answers to these important concerns. Our club provides a sophisticated setting for thoughtful discourse, interdisciplinary projects, and insightful activities. We are committed to bridging the gap between scientific research and religious discovery. Our goal is to understand the depths of spirituality and science together to promote a deeper understanding of both the universe and the self. Come along on this exploration of the ways in which various domains might improve each other's and our lives.</p>
+            <p className="desbg z-10 px-5 rounded-2xl text-[#dfb722] py-10 max-w-[40vw] newamst text-4xl text-center">What is Spirituality ? and What is Science?</p>
+            <p className="desbg z-10 px-5 rounded-2xl py-4 max-w-[40vw] newamst text-2xl text-center">  We at the Science and Spirituality Club are investigating the intersection of knowledge and awareness in an effort to hunt  answers to these important concerns. Our club provides a sophisticated setting for thoughtful discourse, interdisciplinary projects, and insightful activities. We are committed to bridging the gap between scientific research and religious discovery. Our goal is to understand the depths of spirituality and science together to promote a deeper understanding of both the universe and the self. Come along on this exploration of the ways in which various domains might improve each other's and our lives.</p>
         </div>
     )
 }
